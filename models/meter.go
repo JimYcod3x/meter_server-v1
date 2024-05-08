bae7623a48fb982dab85dce773a55d78e4c6d540
JimYcod3x <jimycodex@gmail.com>
2024-05-08 09:15:24 +0800

    test function-mysql-redis

--- models/meter.go
+++ models/meter.go
@@ -3,8 +3,7 @@ package models
 import "gorm.io/gorm"
 
 type Meter struct {
-	ID string `gorm:"unique;primaryKey;autoIncrement"`
-	MeterID string `gorm:"type:varchar(50);unique" json:"meterId"`
+	MeterID string `gorm:"type:varchar(50);primary_key" json:"meterId"`
 	MeterType string `gorm:"type:varchar(50);not null" json:"meterType"`
 	MasterKey string `gorm:"type:varchar(50)"`
 	DataKey string `gorm:"type:varchar(50)"`
